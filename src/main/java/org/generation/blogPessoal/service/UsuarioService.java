3f6aef7ff4b4aa42047a8e759965aec8fc082385
Augusto Cardoso Xavier <augustocx15@gmail.com>
2021-12-14 14:09:34 -0300

    Update UsuarioService.java

--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -4,9 +4,11 @@ import java.nio.charset.Charset;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
+
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.repository.UsuarioRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,7 +22,7 @@ public class UsuarioService {
 	public Usuario cadastrarUsuario(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		String senhaEncoder = encoder.encode(usuario.getSenha());
+		String senhaEncoder = encoder.encode(usuario .getSenha());
 		usuario.setSenha(senhaEncoder);
 		
 		return repository.save(usuario);
@@ -28,23 +30,23 @@ public class UsuarioService {
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional<Usuario> usuario = repository.findByUsuario(user.get().getSenha());
-		
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+
 		if(usuario.isPresent()) {
 			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
-	
+
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
 				
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
-				
+
 				return user;
 			}
 		}
-		
 		return null;
 	}
 }
+
 
